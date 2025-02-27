26edb275fb1b231f70efe660e199b784b3119653
Bart Dorsey <bart@bartdorsey.com>
2025-02-27 15:59:22 -0600

    Fixing some minor flake8 problems

--- popShop.py
+++ popShop.py
@@ -2,57 +2,57 @@ restaurant_menu = {
     "appetizers": [
         {"name": "Salad", "prep_time": 10},
         {"name": "Soup", "prep_time": 15},
-        {"name": "Garlic Bread", "prep_time": 20}
+        {"name": "Garlic Bread", "prep_time": 20},
     ],
     "main_course": [
         {"name": "Steak", "prep_time": 30},
         {"name": "Pasta", "prep_time": 25},
-        {"name": "Fish", "prep_time": 20}
+        {"name": "Fish", "prep_time": 20},
     ],
     "desserts": [
         {"name": "Cake", "prep_time": 40},
         {"name": "Ice Cream", "prep_time": 5},
-        {"name": "Fruit Salad", "prep_time": 15}
-    ]
+        {"name": "Fruit Salad", "prep_time": 15},
+    ],
 }
 restaurant_menu = {
     "appetizers": [
         {"name": "Salad", "prep_time": 10},
         {"name": "Soup", "prep_time": 15},
-        {"name": "Garlic Bread", "prep_time": 20}
+        {"name": "Garlic Bread", "prep_time": 20},
     ],
     "main_course": [
         {"name": "Steak", "prep_time": 30},
         {"name": "Pasta", "prep_time": 25},
-        {"name": "Fish", "prep_time": 20}
+        {"name": "Fish", "prep_time": 20},
     ],
     "desserts": [
         {"name": "Cake", "prep_time": 40},
         {"name": "Ice Cream", "prep_time": 5},
-        {"name": "Fruit Salad", "prep_time": 15}
-    ]
+        {"name": "Fruit Salad", "prep_time": 15},
+    ],
 }
 
 print("Welcome to PopShop!")
-#Enter code to list all appetizers in stock
+# Enter code to list all appetizers in stock
 
 app = int(input("Enter the index of your chosen appetizer:"))
 
 print("\nHere is a list of all of our main courses:")
-#Enter code to list all main courses in stock
+# Enter code to list all main courses in stock
 
 main = int(input("Enter the index of your chosen main course:"))
 
 print("\nHere is a list of all of our desserts:")
-#Enter code to list all desserts in stock
+# Enter code to list all desserts in stock
 
 dessert = int(input("Enter the index of your chosen dessert:"))
 
 total_preptime = 0
-#Enter code to calculate total preptime for selected meal
-#Hint: You do not need a for loop
+# Enter code to calculate total preptime for selected meal
+# Hint: You do not need a for loop
 
-#Enter code to print the final application output. 
+# Enter code to print the final application output.
 
 # As an example:
 # "You have chosen the Salad as a starter,"
